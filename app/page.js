0c77296f21eceefc51ebb95794ad014d32b87a03
tim-nnd <65796225+tim-nnd@users.noreply.github.com>
2023-07-10 17:02:51 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+redirect('www.tiket.com')
+
 export default function Home() {
   return (
     <main className={styles.main}>
