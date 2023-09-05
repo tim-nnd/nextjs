798819bd14f98a3e62eebf8f32326e1e58e6b2b0
tim-nnd <timothy.ananda@gmail.com>
2023-09-05 11:11:32 +0700

    add missing import

--- pages/dummy-page.js
+++ pages/dummy-page.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
