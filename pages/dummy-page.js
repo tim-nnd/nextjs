0dcdccaf8f19f1276e915d01ba26481352043534
tim-nnd <timothy.ananda@gmail.com>
2023-12-18 11:34:34 +0700

    add microphone listener

--- pages/dummy-page.js
+++ pages/dummy-page.js
@@ -19,6 +19,21 @@ export default function Home() {
       console.log("true")
       return true
       })
+
+      function getLocalStream() {
+        navigator.mediaDevices
+          .getUserMedia({ video: false, audio: true })
+          .then((stream) => {
+            window.localStream = stream; // A
+            window.localAudio.srcObject = stream; // B
+            window.localAudio.autoplay = true; // C
+          })
+          .catch((err) => {
+            console.error(`you got an error: ${err}`);
+          });
+      }
+
+      getLocalStream()
   }, [router])
 
   return (
