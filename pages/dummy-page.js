738b5172c83d8a1b66b1fde62789028987c2e550
tim-nnd <timothy.ananda@gmail.com>
2023-12-18 15:16:19 +0700

    add location

--- pages/dummy-page.js
+++ pages/dummy-page.js
@@ -33,7 +33,7 @@ export default function Home() {
           });
       }
 
-      function getDeviceOrientation () {
+      function getDeviceOrientation() {
         if (DeviceOrientationEvent && typeof( DeviceOrientationEvent.requestPermission ) === 'function' ) {
             // (optional) Do something before API request prompt.
             DeviceOrientationEvent.requestPermission()
@@ -47,6 +47,27 @@ export default function Home() {
             alert( "DeviceOrientationEvent is not defined" );
         }
       }
+
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
+      
+      function success(pos) {
+        const crd = pos.coords;
+      
+        console.log("Your current position is:");
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+      }
+      
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options);
 
       getLocalStream()
 
