2145d0cda6387c57138f9b0a6afe0bf22097ba15
tim-nnd <timothy.ananda@gmail.com>
2023-12-18 13:30:04 +0700

    add camera request

--- pages/dummy-page.js
+++ pages/dummy-page.js
@@ -33,7 +33,27 @@ export default function Home() {
           });
       }
 
+      function getDeviceMotion () {
+        if ( typeof( DeviceMotionEvent ) !== "undefined" && typeof( DeviceMotionEvent.requestPermission ) === "function" ) {
+            // (optional) Do something before API request prompt.
+            DeviceMotionEvent.requestPermission()
+                .then( response => {
+                // (optional) Do something after API prompt dismissed.
+                if ( response == "granted" ) {
+                    window.addEventListener( "devicemotion", (e) => {
+                      console.log(`${e.acceleration.x} m/s2`);
+                    })
+                }
+            })
+                .catch( console.error )
+        } else {
+            alert( "DeviceMotionEvent is not defined" );
+        }
+      }
+
       getLocalStream()
+
+      getDeviceMotion()
   }, [router])
 
   return (
