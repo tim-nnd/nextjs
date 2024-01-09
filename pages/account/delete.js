581eb4afd413379537e906d4d1ca3fbb8fade2a9
tim-nnd <timothy.ananda@gmail.com>
2024-01-09 16:54:35 +0700

    add order, account, delete pages

---
+++ pages/account/delete.js
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Delete Page
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="javascript:history.back()"
+          className={styles.card}
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Back <span>-&gt;</span>
+          </h2>
+        </a>
+
+        <a
+          href="https://nextjs-tim-nnd.vercel.app/order"
+          className={styles.card}
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Order <span>-&gt;</span>
+          </h2>
+        </a>
+      </div>
+    </main>
+  )
+}
---
+++ pages/order.js
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Order Page
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs-tim-nnd.vercel.app/account"
+          className={styles.card}
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Account <span>-&gt;</span>
+          </h2>
+        </a>
+
+        <a
+          href="https://nextjs-tim-nnd.vercel.app/account/delete"
+          className={styles.card}
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Delete Account <span>-&gt;</span>
+          </h2>
+        </a>
+      </div>
+    </main>
+  )
+}
